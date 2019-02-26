de90cecb43f7d8c894d072cc5da31062fc451543
neuroneil <47224574+neuroneil@users.noreply.github.com>
2019-02-26 14:59:50 -0500

    Completed table for our plot

---
+++ Script.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+library(readxl)
+
+enrollment_spring_2019 <- read_xlsx("class_enrollment_summary_by_term_2.26.19.xlsx")
+enrollment_spring_2018 <- read_xlsx("class_enrollment_summary_by_term_03.06.18.xlsx")
