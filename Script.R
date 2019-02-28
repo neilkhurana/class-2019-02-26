57cfb3f29c9430de1623cf7e7cf97dfc89dbd544
neuroneil <47224574+neuroneil@users.noreply.github.com>
2019-02-28 13:46:35 -0500

    New HTML, new xlsx

--- Script.R
+++ Script.R
@@ -1,5 +1,21 @@
 library(tidyverse)
 library(readxl)
+library(janitor)
 
-enrollment_spring_2019 <- read_xlsx("class_enrollment_summary_by_term_2.26.19.xlsx")
-enrollment_spring_2018 <- read_xlsx("class_enrollment_summary_by_term_03.06.18.xlsx")
+x_2019 <- read_xlsx("class_enrollment_summary_by_term_2.26.19.xlsx", skip = 3) %>% 
+  clean_names() %>% 
+  filter(! is.na(course_name)) %>% 
+  select(course_id, course_title, course_name, u_grad)
+
+y_2018 <- read_xlsx("class_enrollment_summary_by_term_03.06.18.xlsx", skip = 3) %>% 
+  clean_names() %>% 
+  filter(! is.na(course_name)) %>% 
+  select(course_id, course_title, course_name, u_grad)
+
+z_joined <- inner_join(x_2019, y_2018, by = "course_id") %>% 
+  select(course_title.x, course_name.x, u_grad.y, u_grad.x) %>% 
+  mutate(change = u_grad.x - u_grad.y) %>% 
+  arrange(change) %>% 
+  slice(1:10)
+  
+
